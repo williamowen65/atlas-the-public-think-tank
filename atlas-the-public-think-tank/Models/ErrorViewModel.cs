e90404e8fc643ecebb1c81d4c16a87f293c3c7d1
William Owen <william.owen.bsc@gmail.com>
2025-03-30 09:32:28 -0700

    Add project files.

---
+++ atlas-the-public-think-tank/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace atlas_the_public_think_tank.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
