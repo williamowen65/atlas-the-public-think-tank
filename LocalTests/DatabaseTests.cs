a1576079af62abc6265a6654adb0a72d7defd9b7
William Owen <william.owen.bsc@gmail.com>
2025-04-08 12:33:28 -0700

    Added some test stubs

---
+++ LocalTests/DatabaseTests.cs
@@ -0,0 +1,38 @@
+﻿namespace LocalTests
+{
+    [TestClass]
+    public sealed class DatabaseTests
+    {
+        [TestMethod]
+        [TestCategory("Database")]
+        public void Should_ConnectToDatabase()
+        {
+            // Test logic here
+        }
+
+        [TestMethod]
+        [TestCategory("Database")]
+        public void Should_InsertRecord()
+        {
+            // Test logic here
+        }
+    }
+
+    [TestClass]
+    public sealed class UserTests
+    {
+        [TestMethod]
+        [TestCategory("User")]
+        public void Should_CreateUser()
+        {
+            Assert.Fail("This test is intentionally set to fail.");
+        }
+
+        [TestMethod]
+        [TestCategory("User")]
+        public void Should_DeleteUser()
+        {
+            Assert.Fail("This test is intentionally set to fail.");
+        }
+    }
+}
