d3e2291f04d004993c07fcd0ff102dbd3f5e61f1
William Owen <william.owen.bsc@gmail.com>
2025-04-08 12:16:10 -0700

    Added CloudTests and updated github actions to only run those tests

---
+++ CloudTests/CloudTests/Test1.cs
@@ -0,0 +1,17 @@
+﻿
+/**
+ * CloudTests run in the cloud when pushing to github
+ */
+
+namespace CloudTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
