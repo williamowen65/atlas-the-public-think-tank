eb9372e85e606f65ab772bf8f428a9e1ee4c22d8
William Owen <william.owen.bsc@gmail.com>
2025-05-07 10:02:19 -0700

    Seed data update

--- www.thepublicthinktank.com/Data/SeedData.cs
+++ www.thepublicthinktank.com/Data/SeedData.cs
@@ -11,69 +11,55 @@ namespace atlas_the_public_think_tank.Data
 
             Console.WriteLine("TODO: Seed data");
 
-            //await SeedDefaultRoles(roleManager);
+            await SeedDefaultRoles(roleManager);
          
             //// Seed default users
-            //await SeedDefaultUsers(userManager);
+            await SeedDefaultUsers(userManager);
 
-            //await SeedCategories(context);
+            await SeedCategories(context);
         }
 
         public static async Task SeedCategories(ApplicationDbContext context)
         {
-            // Check if categories already exist
             if (context.Categories.Any())
             {
-                return;   // DB has already been seeded with categories
+                return; // DB has already been seeded with categories
             }
 
-            //var categories = new List<Category>
-            //{
-            //    new Category { 
-            //        Name = "Global Cooperation" ,
-            //        Description = "",
-            //         CreatedAt = DateTime.UtcNow
-            //    },
-            //    new Category { 
-            //        Name = "Sustainable Development" ,
-            //        Description = "",
-            //         CreatedAt = DateTime.UtcNow
-            //    },
-            //    new Category { 
-            //        Name = "Equitable Access" ,
-            //        Description = "",
-            //         CreatedAt = DateTime.UtcNow
-            //    },
-            //    new Category { 
-            //        Name = "Innovation and Technology" ,
-            //        Description = "",
-            //         CreatedAt = DateTime.UtcNow
-            //    },
-            //    new Category { 
-            //        Name = "Effective Governance" ,
-            //        Description = "",
-            //         CreatedAt = DateTime.UtcNow
-            //    },
-            //    new Category { 
-            //        Name = "Education and Awareness" ,
-            //        Description = "",
-            //         CreatedAt = DateTime.UtcNow
-            //    },
-            //    new Category { 
-            //        Name = "Cultural Understanding" ,
-            //        Description = "",
-            //         CreatedAt = DateTime.UtcNow
-            //    },
-            //    new Category { 
-            //        Name = "Resilience and Adaptability",
-            //        Description = "",
-            //         CreatedAt = DateTime.UtcNow
-            //     }
-            //};
+            var categories = new List<Category>
+            {
+            new Category { CategoryName = "Global Cooperation" },
+            new Category { CategoryName = "Sustainable Development" },
+            new Category { CategoryName = "Equitable Access" },
+            new Category { CategoryName = "Innovation and Technology" },
+            new Category { CategoryName = "Effective Governance" },
+            new Category { CategoryName = "Education and Awareness" },
+            new Category { CategoryName = "Cultural Understanding" },
+            new Category { CategoryName = "Resilience and Adaptability" }
+            };
 
-            //context.Categories.AddRange(categories);
+            context.Categories.AddRange(categories);
             await context.SaveChangesAsync();
-        }   
+        }
+
+        public static async Task SeedScopes(ApplicationDbContext context)
+        {
+            if (context.Scopes.Any())
+            {
+                return; // DB has already been seeded with scopes
+            }
+
+            var scopes = new List<Scope>
+        {
+            new Scope { ScopeName = "Global" },
+            new Scope { ScopeName = "National" },
+            new Scope { ScopeName = "Local" },
+            new Scope { ScopeName = "Individual" }
+        };
+
+            context.Scopes.AddRange(scopes);
+            await context.SaveChangesAsync();
+        }
 
         public static async Task SeedDefaultRoles(RoleManager<IdentityRole> roleManager)
         {
