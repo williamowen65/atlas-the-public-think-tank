9f6026228974e19d005ddf574e01636835f6aba3
William Owen <william.owen.bsc@gmail.com>
2025-06-11 10:40:24 -0700

    Refactor based on data-url

--- www.thepublicthinktank.com/wwwroot/js/pagination.js
+++ www.thepublicthinktank.com/wwwroot/js/pagination.js
@@ -13,12 +13,12 @@ function setPaginationButtonListener(paginationButtonElement) {
     if (paginationButtonElement) {
         paginationButtonElement.addEventListener("click", (e => {
 
-            const nextPageNumber = paginationButtonElement.getAttribute("data-next-page")
+            const paginationUrl = paginationButtonElement.getAttribute("data-url")
 
-            if (nextPageNumber) {
+            if (paginationUrl) {
                 // Call api/posts for next page of data
 
-                fetch(`/issue/getPaginatedIssues?currentPage=${nextPageNumber}`)
+                fetch(paginationUrl)
                     .then(response => response.text())
                     .then(data => {
 
@@ -26,8 +26,18 @@ function setPaginationButtonListener(paginationButtonElement) {
                         const domTarget = document.querySelector('#main-content')
                         domTarget.insertAdjacentHTML("beforeend", data)
 
-                        paginationButtonElement.setAttribute("data-next-page", Number(nextPageNumber) + 1)
+                        // Find the current page number and increment it for the next request
+                        const currentPageRegex = /currentPage=(\d+)/;
+                        const match = paginationUrl.match(currentPageRegex);
 
+                        if (match && match[1]) {
+                            const currentPage = parseInt(match[1]);
+                            const nextPage = currentPage + 1;
+                            const newUrl = paginationUrl.replace(currentPageRegex, `currentPage=${nextPage}`);
+                            paginationButtonElement.setAttribute("data-url", newUrl);
+                        } else {
+                            console.error("Could not extract page number from URL");
+                        }
 
                     })
                     .catch(error => {
