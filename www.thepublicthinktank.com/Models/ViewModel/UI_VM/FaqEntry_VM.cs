4b1734e1f086e219f81570b769e06e3f33eabf5b
William Owen <william.owen.career@gmail.com>
2026-03-13 10:48:00 -0700

    Faq info

---
+++ www.thepublicthinktank.com/Models/ViewModel/UI_VM/FaqEntry_VM.cs
@@ -0,0 +1,9 @@
+﻿namespace atlas_the_public_think_tank.Models.ViewModel.UI_VM
+{
+    public class FaqEntry_VM
+    {
+        public required string Id { get; set; }   
+        public required string Question { get; set; }
+        public required string Answer { get; set; }
+    }
+}
