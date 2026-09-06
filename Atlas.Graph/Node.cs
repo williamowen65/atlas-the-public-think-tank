19639fae735b12419201ba88fca69b6332c502a9
William Owen <william.owen.career@gmail.com>
2026-09-05 17:35:27 -0700

    ToString to Value

--- Atlas.Graph/Node.cs
+++ Atlas.Graph/Node.cs
@@ -51,7 +51,7 @@ public sealed class Node
         _domainEvents.Add(
           new NodeCreated(
               Id.Value,
-              DescriptionId.ToString(),
+              DescriptionId.Value,
               createdAt));
     }
 
