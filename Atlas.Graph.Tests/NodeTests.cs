cbd960866d09f848a034687de1ad2750dc9ba5c3
William Owen <66016649+williamowen65@users.noreply.github.com>
2026-09-05 17:38:33 -0700

    test(graph): update node tests for description references

--- Atlas.Graph.Tests/NodeTests.cs
+++ Atlas.Graph.Tests/NodeTests.cs
@@ -1,5 +1,5 @@
-﻿using Atlas.Graph;
 using Atlas.Graph.Nodes;
+using Atlas.Graph.Nodes.NodeTypes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Atlas.Graph.Tests;
@@ -10,43 +10,76 @@ public class NodeTests
     [TestMethod]
     public void Constructor_SetsTitle()
     {
+        var node = CreateNode("Climate adaptation");
+
+        Assert.AreEqual(
+            new NodeTitle("Climate adaptation"),
+            node.Title);
+    }
+
+    [TestMethod]
+    public void Constructor_SetsDescriptionReference()
+    {
+        var descriptionId = new NodeDescriptionId(Guid.NewGuid());
+
         var node = new Node(
-            new NodeTitle("Climate adaption"),
-            NodeType.Question,
+            new NodeTitle("Climate adaptation"),
+            descriptionId,
+            NodeTypeId.New(),
             DateTimeOffset.UtcNow);
 
-        Assert.AreEqual(new NodeTitle("Climate adaption"), node.Title);
+        Assert.AreEqual(descriptionId, node.DescriptionId);
     }
 
     [TestMethod]
     public void Rename_WithValidTitle_ChangesTitle()
     {
-        var node = new Node(
-        new NodeTitle("Climate adaption"),
-        NodeType.Question,
-        DateTimeOffset.UtcNow);
-
+        var node = CreateNode("Climate adaptation");
 
         node.Rename(
             new NodeTitle("Updated title"),
             DateTimeOffset.UtcNow);
 
-        Assert.AreEqual(new NodeTitle("Updated title"), node.Title);
+        Assert.AreEqual(
+            new NodeTitle("Updated title"),
+            node.Title);
     }
 
     [TestMethod]
     public void Rename_WithBlankTitle_ThrowsArgumentException()
     {
-        var node = new Node(
-            new NodeTitle("Climate adaption"),
-            NodeType.Question,
-            DateTimeOffset.UtcNow);
+        var node = CreateNode("Climate adaptation");
 
         Assert.Throws<ArgumentException>(
             () => node.Rename(
                 new NodeTitle("    "),
                 DateTimeOffset.UtcNow));
 
-        Assert.AreEqual(new NodeTitle("Climate adaption"), node.Title);
+        Assert.AreEqual(
+            new NodeTitle("Climate adaptation"),
+            node.Title);
+    }
+
+    [TestMethod]
+    public void ReplaceDescriptionReference_ChangesReference()
+    {
+        var node = CreateNode("Climate adaptation");
+        var replacementId =
+            new NodeDescriptionId(Guid.NewGuid());
+
+        node.ReplaceDescriptionReference(
+            replacementId,
+            DateTimeOffset.UtcNow);
+
+        Assert.AreEqual(replacementId, node.DescriptionId);
+    }
+
+    private static Node CreateNode(string title)
+    {
+        return new Node(
+            new NodeTitle(title),
+            new NodeDescriptionId(Guid.NewGuid()),
+            NodeTypeId.New(),
+            DateTimeOffset.UtcNow);
     }
 }
