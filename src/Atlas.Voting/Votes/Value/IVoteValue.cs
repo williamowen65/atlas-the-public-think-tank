4432704f2b3280e731b026acc89b2179e0529f64
William Owen <66016649+williamowen65@users.noreply.github.com>
2026-09-12 15:47:44 -0700

    PTT-81 move Atlas.Voting/Votes/Value/IVoteValue.cs under src

---
+++ src/Atlas.Voting/Votes/Value/IVoteValue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atlas.Voting.Votes.Value
+{
+    public interface IVoteValue
+    {
+        int Value { get; set; } 
+    }
+}
