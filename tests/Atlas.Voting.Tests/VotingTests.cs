b78db7f64f595c18d5a4c954d5280d4d71700f48
William Owen <66016649+williamowen65@users.noreply.github.com>
2026-09-12 15:47:46 -0700

    PTT-81 move Atlas.Voting.Tests/VotingTests.cs under tests

---
+++ tests/Atlas.Voting.Tests/VotingTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Atlas.Voting.Tests
+{
+    [TestClass]
+    public sealed class VotingTests
+    {
+        [TestMethod]
+        public void CastVote_RequiresAuthenticatedParticipant()
+        {
+            Assert.Fail();
+        }
+    }
+}
