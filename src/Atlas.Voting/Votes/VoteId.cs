c2d992d753f02cf18fcf896fc3585098871a7e5e
William Owen <66016649+williamowen65@users.noreply.github.com>
2026-09-12 15:47:42 -0700

    PTT-81 move Atlas.Voting/Votes/VoteId.cs under src

---
+++ src/Atlas.Voting/Votes/VoteId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atlas.Voting.Votes
+{
+    /// <summary>Identifies a voteId within the Voting boundary.</summary>
+    public readonly record struct VoteId(Guid Value)
+    {
+        public static VoteId New() {
+            return new VoteId(Guid.NewGuid());
+        }
+    }
+}
+
