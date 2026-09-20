02a5b733f4775e83fc8e506e21e437f9a7049005
William Owen <66016649+williamowen65@users.noreply.github.com>
2026-09-20 12:14:16 -0700

    PTT-103 name node importance rating explicitly

---
+++ src/Atlas.Voting/Model/Votes/VoteValue/NodeImportanceRating.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atlas.Voting.Value
+{
+    public class NodeImportanceRating : IVoteValue
+    {
+        public int Value { get;}
+
+        public NodeImportanceRating(int value) {
+
+            if (0 <= value && value <= 10)
+            {
+                Value = value;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(
+                        nameof(value),
+                        value,
+                        "Node importance rating must be between 0 and 10.");
+            }
+        }
+
+
+    }
+}
